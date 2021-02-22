4208ae6ff91f39aadba5c72d85b6329d474fed65
timc <tczerwonka@gmail.com>
2021-02-21 18:57:06 -0600

    initial new files

---
+++ version-2021/sea-222-2021/pindefs.h
@@ -0,0 +1,29 @@
+#ifndef DISPLAY_PINDEFS_H
+#define DISPLAY_PINDEFS_H
+
+/* pindefs.h */
+/* define the pins used in the SEA-DMI ESP504 conversion */
+
+/* It may be reasonable to use the Arduino SPI library and       */
+/* reassign MISO/MOSI and SCK pins to the hardware intended pins */
+
+const int A_0 = 4;        	//23
+const int A_1 = 3;        	//22
+const int A_2 = 2;        	//21
+const int SpiEn = 5;     	//26
+const int LockDet = 6;   	//4
+const int AdcCsOut = 7;  	//8
+const int PttIn = 10;    	//36
+const int MOSILOCAL = 11;     	//32
+const int MISOLOCAL = 12;     	//31
+//const int SCKLOCAL = 13; 	//33
+const int U3SW = 9;
+const int SCKLOCAL = 8;      	//33 moved from 13 to 8 as 13 has the led
+const int LED = 13;
+const int TRUE = 1;
+const int FALSE = 0;
+
+const int BEEP = A0;     	//9
+const int CW = A2;  		//clockwise switch -- 6
+const int CCW = A1; 		//counterclockwise switch -- 5
+
---
+++ version-2021/sea-222-2021/sea-222-2021.ino
@@ -0,0 +1,24 @@
+/*
+ * sea-222-2021.pde
+ * T Czerwonka WO9U
+ * February 2021
+ * https://github.com/tczerwonka/sea-222
+ * 
+ * Project to take the ESP504 radio move it to the US 222 amateur 
+ * band.  The 2022 code (see elsewhere in the project) was minimally
+ * working but really needs to be completely re-written.  
+ * Additionally decent functions for the PLL needed to be written 
+ * then and that's still true today.
+ * Plus the A register for the PLL is 63/64 and that's it.
+ *
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
