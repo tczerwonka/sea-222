99fe994c3048113e907d1277669b1b644e1ec390
timc <tczerwonka@gmail.com>
2021-05-23 22:19:04 -0500

    multiple test files

--- logic-analyzer/esp-fsm-parse.py
+++ logic-analyzer/esp-fsm-parse.py
@@ -12,22 +12,38 @@
 from csv import reader
 import sys
 
-logic_csv = 'ESP504-clean.csv'
-
 
 
 ################################################################################
 #column defines
 ################################################################################
-time_col = 0
-main_spi_en = 1
-A0 = 2
-A1 = 3
-A2 = 4
-SCK = 5
-MOSI = 6
-MISO = 7
-ch7 = 8
+file = 2
+
+#first
+if (file == 1):
+    logic_csv = 'ESP504-clean.csv'
+    time_col = 0
+    main_spi_en = 1
+    A0 = 2
+    A1 = 3
+    A2 = 4
+    SCK = 5
+    MOSI = 6
+    MISO = 7
+    ch7 = 8
+
+#Time [s],EN,SCK,MISO,MOSI,A0-23,A1-22,A2-21,Channel 7
+if (file == 2):
+    logic_csv = 'turnon2.csv'
+    time_col = 0
+    main_spi_en = 1
+    SCK = 2
+    MISO = 3
+    MOSI = 4
+    A0 = 5
+    A1 = 6
+    A2 = 7
+    ch7 = 8
 
 
 
@@ -53,32 +69,32 @@ def read_logic_csv():
                 print(row, end='')
                 if (row[main_spi_en] == '0'):
                     if ((row[A2] == '0') & (row[A1] == '0') & (row[A0] == '0')):
-                        print("UNDEFINED")
+                        print("UNDEFINED0-0")
                     if ((row[A2] == '0') & (row[A1] == '0') & (row[A0] == '1')):
                         decode_in_latch(row)
                     if ((row[A2] == '0') & (row[A1] == '1') & (row[A0] == '0')):
-                        print("UNDEFINED")
+                        print("UNDEFINED2-0")
                     if ((row[A2] == '0') & (row[A1] == '1') & (row[A0] == '1')):
                         print("LED-DTMF-EN")
                     if ((row[A2] == '1') & (row[A1] == '0') & (row[A0] == '0')):
                         print("DSP_LATCH")
                     if ((row[A2] == '1') & (row[A1] == '0') & (row[A0] == '1')):
-                        print("UNDEFINED")
+                        print("UNDEFINED5-0")
                     if ((row[A2] == '1') & (row[A1] == '1') & (row[A0] == '0')):
-                        print("UNDEFINED")
+                        print("UNDEFINED6-0")
                     if ((row[A2] == '1') & (row[A1] == '1') & (row[A0] == '1')):
-                        print("UNDEFINED")
+                        print("UNDEFINED7-0")
                 if (row[main_spi_en] == '1'):
                     if ((row[A2] == '0') & (row[A1] == '0') & (row[A0] == '0')):
-                        print("UNDEFINED")
+                        print("UNDEFINED0-1")
                     if ((row[A2] == '0') & (row[A1] == '0') & (row[A0] == '1')):
-                        print("UNDEFINED")
+                        print("UNDEFINED1-1")
                     if ((row[A2] == '0') & (row[A1] == '1') & (row[A0] == '0')):
-                        print("UNDEFINED")
+                        print("UNDEFINED2-1")
                     if ((row[A2] == '0') & (row[A1] == '1') & (row[A0] == '1')):
                         print("LED-DTMF-EN")
                     if ((row[A2] == '1') & (row[A1] == '0') & (row[A0] == '0')):
-                        print("UNDEFINED")
+                        print("UNDEFINED4-1")
                     if ((row[A2] == '1') & (row[A1] == '0') & (row[A0] == '1')):
                         print("DAC_EN")
                     if ((row[A2] == '1') & (row[A1] == '1') & (row[A0] == '0')):
